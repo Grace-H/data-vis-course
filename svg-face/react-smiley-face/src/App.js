7f5612efa0204f01acab02efb5ce5c0916c71139
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2022-11-12 22:47:05 -0600

    PART3: add smile with d3 arc

--- svg-face/react-smiley-face/src/App.js
+++ svg-face/react-smiley-face/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { arc } from "d3-shape";
 
 export default function App() {
   const width = 960;
@@ -9,27 +10,29 @@ export default function App() {
   const eyeOffsetX = 90;
   const eyeOffsetY = 100;
   const eyeRadius = 50;
+  const mouthRadius = 120;
+  const mouthWidth = 20;
+
+  const mouthArc = arc()
+    .innerRadius(mouthRadius)
+    .outerRadius(mouthWidth + mouthRadius)
+    .startAngle(Math.PI / 2)
+    .endAngle((3 * Math.PI) / 2);
 
   return (
     <svg width={width} height={height}>
-      <circle
-        cx={centerX}
-        cy={centerY}
-        r={height / 2 - strokeWidth / 2}
-        fill="yellow"
-        stroke="black"
-        stroke-width={strokeWidth}
-      />
-      <circle
-        cx={centerX - eyeOffsetX}
-        cy={centerY - eyeOffsetY}
-        r={eyeRadius}
-      />
-      <circle
-        cx={centerX + eyeOffsetX}
-        cy={centerY - eyeOffsetY}
-        r={eyeRadius}
-      />
+      <g transform={`translate(${centerX},${centerY})`}>
+        {/*group element*/}
+        <circle
+          r={height / 2 - strokeWidth / 2}
+          fill="yellow"
+          stroke="black"
+          stroke-width={strokeWidth}
+        />
+        <circle cx={-eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius} />
+        <circle cx={eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius} />
+        <path d={mouthArc()}></path>
+      </g>
     </svg>
   );
 }
