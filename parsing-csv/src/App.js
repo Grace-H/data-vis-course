94d4311fed88b12c5d0f8e8533f5bb78200f08a3
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2024-07-09 15:15:22 -0700

    4:43:05: use d3 pie instead of svg arcs

--- parsing-csv/src/App.js
+++ parsing-csv/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import { csv, arc } from "d3";
+import { csv, arc, pie } from "d3";
 
 const csvURL =
   "https://gist.githubusercontent.com/Grace-H/6fa383b8274ae4de60bd9a2638455854/raw/namedColorsCSS.csv";
@@ -11,6 +11,7 @@ const centerX = width / 2;
 const centerY = height / 2;
 
 const pieArc = arc().innerRadius(0).outerRadius(200);
+const colorPie = pie().value(1);
 
 export default function App() {
   const [data, setData] = useState(null);
@@ -25,14 +26,8 @@ export default function App() {
   return (
     <svg width={width} height={height}>
       <g transform={`translate(${centerX},${centerY})`}>
-        {data.map((d, i) => (
-          <path
-            d={pieArc({
-              startAngle: (i / data.length) * 2 * Math.PI,
-              endAngle: ((i + 1) / data.length) * 2 * Math.PI,
-            })}
-            fill={d["RGB hex value"]}
-          />
+        {colorPie(data).map((d) => (
+          <path d={pieArc(d)} fill={d.data["RGB hex value"]} />
         ))}
       </g>
     </svg>
